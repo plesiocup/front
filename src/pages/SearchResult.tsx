843a3df6b277b3819fd0c47b8988cdbedf88f9ec
smaru1111 <marusou20@gmail.com>
2023-12-04 22:54:49 +0900

    [feat] routing & shadcn導入した

---
+++ src/pages/SearchResult.tsx
@@ -0,0 +1,9 @@
+function SearchResult() {
+  return (
+    <div>
+      <h1>SearchResult</h1>
+    </div>
+  );
+}
+
+export default SearchResult;
---
+++ src/pages/Test.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function Test() {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default Test;
